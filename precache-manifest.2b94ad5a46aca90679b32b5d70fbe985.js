97995f823d8fd3059791a075d6a5aa7245c2376d
artem <70851610+artemdov@users.noreply.github.com>
2021-01-12 16:49:46 +0300

    Updates

---
+++ precache-manifest.2b94ad5a46aca90679b32b5d70fbe985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254689be9f0651500b9147fdcfe85e51",
+    "url": "/IgnatTasks/index.html"
+  },
+  {
+    "revision": "a863f8fb1212e723facf",
+    "url": "/IgnatTasks/static/css/main.9221976f.chunk.css"
+  },
+  {
+    "revision": "7d181372615643de9ee2",
+    "url": "/IgnatTasks/static/js/2.6f1c869c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/IgnatTasks/static/js/2.6f1c869c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a863f8fb1212e723facf",
+    "url": "/IgnatTasks/static/js/main.3cc9fc97.chunk.js"
+  },
+  {
+    "revision": "7a556a60707f984e8cf7",
+    "url": "/IgnatTasks/static/js/runtime-main.775b33e3.js"
+  }
+]);
